6b68109211277ec2bf60b0bef84c4471004c3e11
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-07 13:07:34 +0300

    example011.1_адаптирование предыдущей задачи с использованием метода

---
+++ Example011.1_ArrayLidrary/Program.cs
@@ -0,0 +1,49 @@
+﻿void FillArray(int[] collection) //название элемента "int[] collection"
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+      collection[index] = new Random().Next(1, 10);
+      index++;
+    }
+}
+void PrintArray(int[]col) //"void" позиция, если метод ничего не возвращает
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int faind)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+       if(collection[index] == faind)
+       {
+            position = index;
+            break;
+       }
+       index++; 
+    }
+    return position;
+}
+
+int[] array = new int[10]; //Сначала определили массив из 10 чисел
+
+FillArray(array); //Вызвавли метод "FillArray", который заполнил нам этот массив
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array); //И метод "PrintArray" нам его распечатал
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
